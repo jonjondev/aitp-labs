8394e87cff88e6331706459272774effe5f482dc
Jonathan Moallem <jdmoallem@gmail.com>
2018-09-10 00:12:45 -0400

    Commented index.php

--- IT207/index.php
+++ IT207/index.php
@@ -1,3 +1,9 @@
+<!--
+index.php
+Jonathan Moallem 09/09/2018
+
+This is the home page for the site.
+-->
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
          "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
