636d93197c9d542a71639cb7469705dcf51c9989
Jonathan Moallem <jdmoallem@gmail.com>
2018-09-11 16:24:38 -0400

    Added left column to index content

--- IT207/content.php
+++ IT207/content.php
@@ -4,4 +4,24 @@ Jonathan Moallem 09/10/2018
 
 The home page content. For being called and displayed by a layout.
 -->
-<h1>This is the index page</h1>
+<div class="index-left-col">
+    <div class="index-photo-section"></div>
+    <div class="index-summary-section minimal-padding">
+        <h4>Summary</h4>
+        <ul>
+            <li>Personal</li>
+            <ul>
+                <li>Born and raised in Sydney Australia</li>
+                <li>Mother and father and Hungarian and Iranian respectively</li>
+            </ul>
+            <li>Academic</li>
+            <ul>
+                <li>Studying at The University of Technology Sydney (UTS)</li>
+                <li><b>Degree:</b> B. Sci. IT Dip. IT Prof. Prac.</li>
+                <li><b>Submajors:</b> Enterprise Systems Development, Interaction Design, Games Development</li>
+                <li>Academic exchange at George Mason University (GMU)</li>
+            </ul>
+        </ul>
+    </div>
+</div>
+</div>
