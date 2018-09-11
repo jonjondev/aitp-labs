010b619eadd78e3c2910c9ec10e1a7f9d322a492
Jonathan Moallem <jdmoallem@gmail.com>
2018-09-11 17:27:45 -0400

    Made site DTD strict compliant

--- IT207/content.php
+++ IT207/content.php
@@ -10,29 +10,33 @@ The home page content. For being called and displayed by a layout.
         <h4>Summary</h4>
         <ul>
             <li>Personal</li>
-            <ul>
-                <li>Born and raised in Sydney Australia</li>
-                <li>Mother and father and Hungarian and Iranian respectively</li>
-            </ul>
+            <li class="sub-point">
+                <ul>
+                    <li>Born and raised in Sydney Australia</li>
+                    <li>Mother and father and Hungarian and Iranian respectively</li>
+                </ul>
+            </li>
             <li>Academic</li>
-            <ul>
-                <li>Studying at The University of Technology Sydney (UTS)</li>
-                <li><b>Degree:</b> B. Sci. IT Dip. IT Prof. Prac.</li>
-                <li><b>Submajors:</b> Enterprise Systems Development, Interaction Design, Games Development</li>
-                <li>Academic exchange at George Mason University (GMU)</li>
-            </ul>
+            <li class="sub-point">
+                <ul>
+                    <li>Studying at The University of Technology Sydney (UTS)</li>
+                    <li><b>Degree:</b> B. Sci. IT Dip. IT Prof. Prac.</li>
+                    <li><b>Submajors:</b> Enterprise Systems Development, Interaction Design, Games Development</li>
+                    <li>Academic exchange at George Mason University (GMU)</li>
+                </ul>
+            </li>
         </ul>
     </div>
 </div>
 
 <div class="index-right-col minimal-padding">
-    <h2 class="text-center">Professional & Personal Details</h2>
+    <h2 class="text-center">Professional &amp; Personal Details</h2>
     <h3>Professional</h3>
     <p>I love making things, have a great deal of passion for software development and love learning about new technologies.</p>
     <p>I have worked in software development since finishing high school, including working as a Ruby on Rails developer, lead Android developer for a startup, an app development contractor, and a university programming tutor.</p>
     <p>Currently I am working with a startup to build out their new platform going into the future as well as working on some of my own open-source API tools in JavaScript, Ruby and Crystal.</p>
     <p class="fluid-text">
-        See my <a href="https://www.linkedin.com/in/jmo63/" target="_blank">LinkedIn</a> or <a href="https://j-mo63.github.io" target="_blank">GitHub Pages</a> for additional details.
+        See my <a href="https://www.linkedin.com/in/jmo63/">LinkedIn</a> or <a href="https://j-mo63.github.io">GitHub Pages</a> for additional details.
     </p>
     <h3>Personal</h3>
     <p>I spend a lot of my free time learning new technologies, reading, playing frisbee, futsal or football, and watching television.</p>
