069838b5e06c5f5e9d224ef632cd424f8be64be4
Jonathan Moallem <jdmoallem@gmail.com>
2018-09-25 14:47:03 -0400

    Fixed form styling

--- IT207/LAB1/index_content.php
+++ IT207/LAB1/index_content.php
@@ -4,10 +4,9 @@ Jonathan Moallem 09/09/2018
 
 The LAB1 page content. For being called and displayed by a layout.
 -->
-<div class="lab1-container">
-    <?php
-        echo "<form action='./finalgrade.php' method='post'>";
-    ?>
+<?php
+echo "<form action='./finalgrade.php' method='post'>";
+?>
     <fieldset>
         <h3>Participation</h3>
         <span>Earned:</span> <input type="text" name="earnedParticipation" />
@@ -33,8 +32,6 @@ The LAB1 page content. For being called and displayed by a layout.
         <span>Weight (percentage):</span> <input type="text" name="weightPracticum" />
     </fieldset>
     <fieldset>
-        <br /><br />
         <input type="submit" />
     </fieldset>
-    </form>
-</div>
+</form>
