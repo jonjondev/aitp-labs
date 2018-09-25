8c3fe14f983b8cbce4d95872fcc12689506de150
Jonathan Moallem <jdmoallem@gmail.com>
2018-09-25 13:43:20 -0400

    Added weighting

--- IT207/LAB1/finalgrade_content.php
+++ IT207/LAB1/finalgrade_content.php
@@ -4,8 +4,13 @@
         return ($earned / $maximum) * 100;
     }
 
-    $earned_participation = $_POST['earnedParticipation'];
-    $max_participation = $_POST['maxParticipation'];
-    echo "<p>You earned a ", round(get_percentage($earned_participation, $max_participation), 2),"% for Participation, with a weighted value of ", 2, "%</p>";
+    function get_weighted_percentage($percentage, $weight) {
+        return ($weight / $percentage);
+    }
+
+    $participation_percentage = get_percentage(
+        $_POST['earnedParticipation'], $_POST['maxParticipation']);
+    $participation_weighted = get_weighted_percentage($participation_percentage, $_POST['weightParticipation'])
+    echo "<p>You earned a ", round($participation_percentage, 2),"% for Participation, with a weighted value of ", round($participation_weighted, 2), "%</p>";
     ?>
 </div>
