6571a759ae81cd3457dc075342cc832ee3e455e6
Jonathan Moallem <jdmoallem@gmail.com>
2018-09-25 13:25:17 -0400

    Added percentage calculation

--- IT207/LAB1/finalgrade_content.php
+++ IT207/LAB1/finalgrade_content.php
@@ -1,4 +1,11 @@
-<?php
-    $test = $_POST['earnedParticipation'];
-    echo $test;
-?>
+<div class="lab1-container">
+    <?php
+    function get_percentage($earned, $maximum) {
+        return ($earned / $maximum) * 100;
+    }
+
+    $earned_participation = $_POST['earnedParticipation'];
+    $max_participation = $_POST['maxParticipation'];
+    echo "You earned a ", get_percentage($earned_participation, $max_participation),"% for Participation, with a weighted value of ", $test, "%";
+    ?>
+</div>
