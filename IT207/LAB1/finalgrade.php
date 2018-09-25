a70e5ea30bd244b1eb9b7f6a7cecb833140c435a
Jonathan Moallem <jdmoallem@gmail.com>
2018-09-25 13:29:34 -0400

    Put content in para tags

--- IT207/LAB1/finalgrade.php
+++ IT207/LAB1/finalgrade.php
@@ -7,7 +7,7 @@ The page definition for the LAB1 final grade page. Calls the layout, and tells i
 <?php
 $page_title = "IT 207 Final Grade Determiner";
 $page_name = "./finalgrade_content.php";
-$css_path = "./styles.css";
+$css_path = "styles.css";
 # Set default relative directory for testing
 $current_relative_dir = "..";
 include("../layout.php");
