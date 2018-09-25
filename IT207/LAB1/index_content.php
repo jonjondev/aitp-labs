0110525e752568b953efb96d02f8f6b786b40e55
Jonathan Moallem <jdmoallem@gmail.com>
2018-09-25 14:45:38 -0400

    Placed all fields into fieldsets

--- IT207/LAB1/index_content.php
+++ IT207/LAB1/index_content.php
@@ -8,23 +8,33 @@ The LAB1 page content. For being called and displayed by a layout.
     <?php
         echo "<form action='./finalgrade.php' method='post'>";
     ?>
+    <fieldset>
         <h3>Participation</h3>
         <span>Earned:</span> <input type="text" name="earnedParticipation" />
         <span>Max:</span> <input type="text" name="maxParticipation" />
         <span>Weight (percentage):</span> <input type="text" name="weightParticipation" />
+    </fieldset>
+    <fieldset>
         <h3>Quizzes</h3>
         <span>Earned:</span> <input type="text" name="earnedQuiz" />
         <span>Max:</span> <input type="text" name="maxQuiz" />
         <span>Weight (percentage):</span> <input type="text" name="weightQuiz" />
+    </fieldset>
+    <fieldset>
         <h3>Lab Assignments</h3>
         <span>Earned:</span> <input type="text" name="earnedLab" />
         <span>Max:</span> <input type="text" name="maxLab" />
         <span>Weight (percentage):</span> <input type="text" name="weightLab" />
+    </fieldset>
+    <fieldset>
         <h3>Practica</h3>
         <span>Earned:</span> <input type="text" name="earnedPracticum" />
         <span>Max:</span> <input type="text" name="maxPracticum" />
         <span>Weight (percentage):</span> <input type="text" name="weightPracticum" />
+    </fieldset>
+    <fieldset>
         <br /><br />
         <input type="submit" />
+    </fieldset>
     </form>
 </div>
