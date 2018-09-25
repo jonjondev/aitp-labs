0aa9a020c2e08f914725b3cbd4ccf012083285e9
Jonathan Moallem <jdmoallem@gmail.com>
2018-09-25 14:49:02 -0400

    Changed fieldsets to divs

--- IT207/LAB1/index_content.php
+++ IT207/LAB1/index_content.php
@@ -4,34 +4,37 @@ Jonathan Moallem 09/09/2018
 
 The LAB1 page content. For being called and displayed by a layout.
 -->
-<?php
-echo "<form action='./finalgrade.php' method='post'>";
-?>
-    <fieldset>
-        <h3>Participation</h3>
-        <span>Earned:</span> <input type="text" name="earnedParticipation" />
-        <span>Max:</span> <input type="text" name="maxParticipation" />
-        <span>Weight (percentage):</span> <input type="text" name="weightParticipation" />
-    </fieldset>
-    <fieldset>
-        <h3>Quizzes</h3>
-        <span>Earned:</span> <input type="text" name="earnedQuiz" />
-        <span>Max:</span> <input type="text" name="maxQuiz" />
-        <span>Weight (percentage):</span> <input type="text" name="weightQuiz" />
-    </fieldset>
-    <fieldset>
-        <h3>Lab Assignments</h3>
-        <span>Earned:</span> <input type="text" name="earnedLab" />
-        <span>Max:</span> <input type="text" name="maxLab" />
-        <span>Weight (percentage):</span> <input type="text" name="weightLab" />
-    </fieldset>
-    <fieldset>
-        <h3>Practica</h3>
-        <span>Earned:</span> <input type="text" name="earnedPracticum" />
-        <span>Max:</span> <input type="text" name="maxPracticum" />
-        <span>Weight (percentage):</span> <input type="text" name="weightPracticum" />
-    </fieldset>
-    <fieldset>
-        <input type="submit" />
-    </fieldset>
-</form>
+<div class="lab1-container">
+    <?php
+    echo "<form action='./finalgrade.php' method='post'>";
+    ?>
+        <div>
+            <h3>Participation</h3>
+            <span>Earned:</span> <input type="text" name="earnedParticipation" />
+            <span>Max:</span> <input type="text" name="maxParticipation" />
+            <span>Weight (percentage):</span> <input type="text" name="weightParticipation" />
+        </div>
+        <div>
+            <h3>Quizzes</h3>
+            <span>Earned:</span> <input type="text" name="earnedQuiz" />
+            <span>Max:</span> <input type="text" name="maxQuiz" />
+            <span>Weight (percentage):</span> <input type="text" name="weightQuiz" />
+        </div>
+        <div>
+            <h3>Lab Assignments</h3>
+            <span>Earned:</span> <input type="text" name="earnedLab" />
+            <span>Max:</span> <input type="text" name="maxLab" />
+            <span>Weight (percentage):</span> <input type="text" name="weightLab" />
+        </div>
+        <div>
+            <h3>Practica</h3>
+            <span>Earned:</span> <input type="text" name="earnedPracticum" />
+            <span>Max:</span> <input type="text" name="maxPracticum" />
+            <span>Weight (percentage):</span> <input type="text" name="weightPracticum" />
+        </div>
+        <div>
+            <br /><br />
+            <input type="submit" />
+        </div>
+    </form>
+</div>
