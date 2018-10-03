b4669c03a4bbc27ca67597054e48fbd358425f98
Jonathan Moallem <jdmoallem@gmail.com>
2018-10-03 16:48:34 -0400

    Added dummy lab2 content

---
+++ IT207/LAB2/index.php
@@ -0,0 +1,13 @@
+<!--
+index.php
+Jonathan Moallem 09/09/2018
+
+The page definition for the LAB2 page. Calls the layout, and tells it to load the LAB2 page content.
+-->
+<?php
+$page_title = "Office Hours Signup";
+$page_name = "./index_content.php";
+$css_path = "./styles.css";
+$current_relative_dir = "..";
+include("../layout.php");
+?>
---
+++ IT207/LAB2/index_content.php
@@ -0,0 +1,9 @@
+<!--
+index_content.php
+Jonathan Moallem 09/09/2018
+
+The LAB2 page content. For being called and displayed by a layout.
+-->
+<div class="lab2-container">
+    <p>LAB 2</p>
+</div>
