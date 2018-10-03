138cac2030b28df244075233eddb532165ca3996
Jonathan Moallem <jdmoallem@gmail.com>
2018-10-03 17:55:01 -0400

    Added calendar to lab2

--- IT207/LAB2/index_content.php
+++ IT207/LAB2/index_content.php
@@ -6,6 +6,187 @@ The LAB2 page content. For being called and displayed by a layout.
 -->
 <div class="lab2-container">
     <div class="lab2-cal-header">
-        <h4>February, 2015</h4>
+        <h4 class="title-text">February, 2015</h4>
+    </div>
+    <div class="lab2-day-boxes">
+        <div class="lab2-day-box">
+            <p class="day-text">Sunday</p>
+        </div>
+        <div class="lab2-day-box">
+            <p class="day-text">Monday</p>
+        </div>
+        <div class="lab2-day-box">
+            <p class="day-text">Tuesday</p>
+        </div>
+        <div class="lab2-day-box">
+            <p class="day-text">Wednesday</p>
+        </div>
+        <div class="lab2-day-box">
+            <p class="day-text">Thursday</p>
+        </div>
+        <div class="lab2-day-box">
+            <p class="day-text">Friday</p>
+        </div>
+        <div class="lab2-day-box">
+            <p class="day-text">Saturday</p>
+        </div>
+    </div>
+    <div class="lab2-date-boxes">
+        <div class="lab2-date-box"></div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">1</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">2</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">3</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">4</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">5</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">6</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">7</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">8</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">9</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">10</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">11</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">12</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">13</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">14</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">15</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">16</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">17</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">18</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">19</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">20</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">21</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">22</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">23</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">24</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">25</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">26</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">27</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">28</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">29</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">30</p>
+            </div>
+        </div>
+        <div class="lab2-date-box">
+            <div class="lab2-date-box-inner">
+                <p class="date-text">31</p>
+            </div>
+        </div>
     </div>
 </div>
