61b63e3c0270d36c774fd9f06c6956e519824230
Jonathan Moallem <jdmoallem@gmail.com>
2018-10-03 16:55:27 -0400

    Added calendar header

--- IT207/LAB2/index_content.php
+++ IT207/LAB2/index_content.php
@@ -5,5 +5,7 @@ Jonathan Moallem 09/09/2018
 The LAB2 page content. For being called and displayed by a layout.
 -->
 <div class="lab2-container">
-    <p>LAB 2</p>
+    <div class="lab2-cal-header">
+        <h4>February, 2015</h4>
+    </div>
 </div>
