61425522e1d4f3a102d329c91bd2be6145f7ba52
Jonathan Moallem <jdmoallem@gmail.com>
2018-10-11 14:01:39 -0400

    Added missing timebox

--- IT207/LAB2/index_content.php
+++ IT207/LAB2/index_content.php
@@ -10,7 +10,7 @@ The LAB2 setup page content. For being called and displayed by a layout.
     </div>
     <div class="lab2-day-boxes">
         <div class="lab2-time-box">
-            <p class="day-text"><b>Day:</b></p>
+            <p class="day-text">Day:</p>
         </div>
         <div class="lab2-time-box">
             <p class="day-text">Monday</p>
@@ -29,7 +29,7 @@ The LAB2 setup page content. For being called and displayed by a layout.
         </div>
     </div>
     <div class="lab2-date-boxes">
-        <div class='lab2-date-box'>
+        <div class='lab2-time-box'>
             <div class='lab2-date-box-inner'>
                 <p>Time:</p>
             </div>
