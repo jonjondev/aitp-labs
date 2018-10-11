a190ebac1710e0fa1e822daab14be2630d2ccb78
Jonathan Moallem <jdmoallem@gmail.com>
2018-10-11 13:59:21 -0400

    Changed to using time boxes

--- IT207/LAB2/index_content.php
+++ IT207/LAB2/index_content.php
@@ -9,52 +9,52 @@ The LAB2 setup page content. For being called and displayed by a layout.
         <h1 class="title-text">Office Hours Setup Form</h1>
     </div>
     <div class="lab2-day-boxes">
-        <div class="lab2-day-box">
+        <div class="lab2-time-box">
             <p class="day-text"><b>Day:</b></p>
         </div>
-        <div class="lab2-day-box">
+        <div class="lab2-time-box">
             <p class="day-text">Monday</p>
         </div>
-        <div class="lab2-day-box">
+        <div class="lab2-time-box">
             <p class="day-text">Tuesday</p>
         </div>
-        <div class="lab2-day-box">
+        <div class="lab2-time-box">
             <p class="day-text">Wednesday</p>
         </div>
-        <div class="lab2-day-box">
+        <div class="lab2-time-box">
             <p class="day-text">Thursday</p>
         </div>
-        <div class="lab2-day-box">
+        <div class="lab2-time-box">
             <p class="day-text">Friday</p>
         </div>
     </div>
     <div class="lab2-date-boxes">
         <div class='lab2-date-box'>
-            <div class='lab2-date-box-inner text-center'>
-                <b>Time:</b>
-            </div>
-        </div>
-        <div class='lab2-date-box'>
             <div class='lab2-date-box-inner'>
                 <p>Time:</p>
             </div>
         </div>
-        <div class='lab2-date-box'>
+        <div class='lab2-time-box'>
             <div class='lab2-date-box-inner'>
                 <p>Time:</p>
             </div>
         </div>
-        <div class='lab2-date-box'>
+        <div class='lab2-time-box'>
             <div class='lab2-date-box-inner'>
                 <p>Time:</p>
             </div>
         </div>
-        <div class='lab2-date-box'>
+        <div class='lab2-time-box'>
             <div class='lab2-date-box-inner'>
                 <p>Time:</p>
             </div>
         </div>
-        <div class='lab2-date-box'>
+        <div class='lab2-time-box'>
+            <div class='lab2-date-box-inner'>
+                <p>Time:</p>
+            </div>
+        </div>
+        <div class='lab2-time-box'>
             <div class='lab2-date-box-inner'>
                 <p>Time:</p>
             </div>
