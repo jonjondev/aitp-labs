8fb0294af90d95210baf8bb3da0c349849ac5be9
Jonathan Moallem <jdmoallem@gmail.com>
2018-10-20 14:08:11 -0400

    Made system track user input dates

--- IT207/LAB2/calendar_content.php
+++ IT207/LAB2/calendar_content.php
@@ -5,95 +5,110 @@ Jonathan Moallem 10/11/2018
 The LAB2 calendar page content. For being called and displayed by a layout.
 -->
 <div class="lab2-container">
-    <div class="lab2-cal-header">
-        <h1 class="title-text">Student Hours Signup</h1>
-        <form action='./meme.php' method='post'>
+    <form action='./calendar.php' method='post'>
+        <div class="lab2-cal-header">
+            <h1 class="title-text">Student Hours Signup</h1>
             <div>
-                <span>Student Name:</span> <input type="text" name="studentName" />
-                <span>Student Email:</span> <input type="text" name="studentEmail" />
+                <?php
+                echo "<span>Student Name:</span> <input type='text' name='studentName' />";
+                echo "<span>Student Email:</span> <input type='text' name='studentEmail' />";
+                ?>
                 <input type="submit" />
                 <input type="submit" value="Clear" />
             </div>
-        </form>
-        <h4 class="title-text">
+            <h4 class="title-text">
+                <?php
+                echo date('F, Y')
+                ?>
+            </h4>
+        </div>
+        <div class="lab2-day-boxes">
+            <div class="lab2-day-box">
+                <p class="day-text">Sunday</p>
+            </div>
+            <div class="lab2-day-box">
+                <p class="day-text">Monday</p>
+            </div>
+            <div class="lab2-day-box">
+                <p class="day-text">Tuesday</p>
+            </div>
+            <div class="lab2-day-box">
+                <p class="day-text">Wednesday</p>
+            </div>
+            <div class="lab2-day-box">
+                <p class="day-text">Thursday</p>
+            </div>
+            <div class="lab2-day-box">
+                <p class="day-text">Friday</p>
+            </div>
+            <div class="lab2-day-box">
+                <p class="day-text">Saturday</p>
+            </div>
+        </div>
+        <div class="lab2-date-boxes">
             <?php
-            echo date('F, Y')
-            ?>
-        </h4>
-    </div>
-    <div class="lab2-day-boxes">
-        <div class="lab2-day-box">
-            <p class="day-text">Sunday</p>
-        </div>
-        <div class="lab2-day-box">
-            <p class="day-text">Monday</p>
-        </div>
-        <div class="lab2-day-box">
-            <p class="day-text">Tuesday</p>
-        </div>
-        <div class="lab2-day-box">
-            <p class="day-text">Wednesday</p>
-        </div>
-        <div class="lab2-day-box">
-            <p class="day-text">Thursday</p>
-        </div>
-        <div class="lab2-day-box">
-            <p class="day-text">Friday</p>
-        </div>
-        <div class="lab2-day-box">
-            <p class="day-text">Saturday</p>
-        </div>
-    </div>
-    <div class="lab2-date-boxes">
-        <?php
-        function get_appointment_controls($events) {
-            $return_string = "<div class='appointment-controls'>";
-            if ($events) {
-                foreach ($events as $time) {
-                    $return_string .= "<input type='checkbox' name='time' />
-                        <label for='time'>$time</label>
-                        <br />";
+            function get_appointment_controls($date, $events) {
+                $return_string = "<div class='appointment-controls'>";
+                if ($events) {
+                    foreach ($events as $time) {
+                        $checkbox_value = $date.'-'.$time;
+                        if ($_POST[APPOINTMENTS_KEY] && 
+                            in_array($checkbox_value, $_POST[APPOINTMENTS_KEY])) {
+                            
+                            $student = $_POST['studentName'];
+                            $return_string .= "<span class='appointment-text'>
+                                $time - $student</span><br />";
+                        }
+                        else {
+                            $appointments_key = APPOINTMENTS_KEY;
+                            $return_string .= "<input type='checkbox' 
+                                                      name='{$appointments_key}[]' 
+                                                      value='$checkbox_value' />
+                                <label for='time'>$time</label>
+                                <br />";
+                        }
+                    }
+                }
+                $return_string .= "</div>";
+                return $return_string;
+            }
+
+            function make_date_box($date, $events) {
+                $appointment_controls = get_appointment_controls($date, $events);
+                echo "<div class='lab2-date-box'>
+                        <div class='lab2-date-box-inner'>
+                            <p class='date-text'>$date</p>
+                            $appointment_controls
+                        </div>
+                    </div>";
+            }
+
+            function make_blank_box() {
+                echo "<div class='lab2-date-box'></div>";
+            }
+
+            $number_of_days = date('t');
+            $first_day = date('01-m-Y');
+            $offset = date('w', strtotime($first_day));
+
+            for ($i = 0; $i < $offset; $i++) {
+                make_blank_box();
+            }
+
+            for ($i = 1; $i <= $number_of_days; $i++) {
+                $current_day = date("$i-m-Y");
+                $week_day = date('w', strtotime($current_day));
+                make_date_box($i, $_POST["day_$week_day"]);
+            }
+
+            for ($i = 1; $i <= 5; $i++) {
+                if ($_POST["day_$i"]) {
+                    foreach($_POST["day_$i"] as $value) {
+                        echo "<input type='hidden' name='day_{$i}[]' value='$value'>";
+                    }
                 }
             }
-            $return_string .= "</div>";
-            return $return_string;
-
-            // return "<div class='appointment-controls'>
-            //             <input type='checkbox' name='time' />
-            //             <label for='time'>7:00am</label>
-            //             <br />
-            //             <span class='appointment-text'>7:30am - Jon</span>
-            //             <br />
-            //         </div>";
-        }
-
-        function make_date_box($date, $events) {
-            $appointment_controls = get_appointment_controls($events);
-            echo "<div class='lab2-date-box'>
-                    <div class='lab2-date-box-inner'>
-                        <p class='date-text'>$date</p>
-                        $appointment_controls
-                    </div>
-                </div>";
-        }
-
-        function make_blank_box() {
-            echo "<div class='lab2-date-box'></div>";
-        }
-
-        $number_of_days = date('t');
-        $first_day = date('01-m-Y');
-        $offset = date('w', strtotime($first_day));
-
-        for ($i = 0; $i < $offset; $i++) {
-            make_blank_box();
-        }
-
-        for ($i = 1; $i <= $number_of_days; $i++) {
-            $current_day = date("$i-m-Y");
-            $week_day = date('w', strtotime($current_day));
-            make_date_box($i, $_POST["day_$week_day"]);
-        }
-        ?>
-    </div>
+            ?>
+        </div>
+    </form>
 </div>
